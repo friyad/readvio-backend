e94c44d985211269903366eb97c3493b720b753a
FriyadDev <riyadhasanblue2323@gmail.com>
2025-10-27 02:22:40 +0600

    refactor: Mongoose connection caching to prevent buffering time out

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,6 @@
 import { app } from "../src/app.js";
+import { connectToDatabase } from "../src/config/db.js";
+
+await connectToDatabase();
 
 export default app;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 import { env } from "./env.js";
 
+declare global {
+  // eslint-disable-next-line no-var
+  var __mongooseConn__: Promise<typeof mongoose> | undefined;
+}
+
 export async function connectToDatabase(): Promise<void> {
-  const connection = await mongoose.connect(env.MONGO_URI, {
-    serverSelectionTimeoutMS: 10000,
-    // socketTimeoutMS: 45000, // optional tuning
-  });
+  if (!global.__mongooseConn__) {
+    global.__mongooseConn__ = mongoose.connect(env.MONGO_URI, {
+      serverSelectionTimeoutMS: 10000,
+    });
+  }
+  const connection = await global.__mongooseConn__;
 
   mongoose.connection.on("disconnected", () => {
     // eslint-disable-next-line no-console
