9a18af5a125dd8af1ddd85d49c68945be5c1cff3
FriyadDev <riyadhasanblue2323@gmail.com>
2025-10-26 23:56:41 +0600

    ci: vercel configurations updated

--- api/index.ts
+++ api/index.ts
@@ -1,24 +1,3 @@
-import { Request, Response } from "express";
-import { app } from "../src/app";
-import { connectToDatabase } from "../src/config/db";
-import http from "http";
+import app from "../src/app";
 
-let server: http.Server | null = null;
-let initialized = false;
-
-async function ensureInitialized(): Promise<void> {
-  if (!initialized) {
-    await connectToDatabase();
-    server = http.createServer(app);
-    initialized = true;
-  }
-}
-
-export default async function handler(req: Request, res: Response) {
-  await ensureInitialized();
-  if (!server) {
-    res.status(500).send("Server not initialized");
-    return;
-  }
-  (server as any).emit("request", req, res);
-}
+export default app;
